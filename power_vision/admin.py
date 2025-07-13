a371d46aeb4278e6d75a4ec583e3c8433c61df86
LucianoMesquitaTech <L11c14j30j20m21>
2025-07-13 17:34:01 -0300

    13-07-domingo

--- power_vision/admin.py
+++ power_vision/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Sensor, Leitura  # substitua pelos seus modelos
+from .models import Usuario, Medicao  # substitua pelos seus modelos
 
-admin.site.register(Sensor)
-admin.site.register(Leitura)
+admin.site.register(Usuario)
+admin.site.register(Medicao)
 
 # Register your models here.
